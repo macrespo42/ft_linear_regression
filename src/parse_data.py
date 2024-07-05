d8db6420decb583eaaef1698f4654250a3f1c6c4
macrespo42 <macrespo@student.42.fr>
2024-07-05 22:48:38 +0200

    feat: LinearRegression model working out of the box

--- src/parse_data.py
+++ src/parse_data.py
@@ -18,13 +18,23 @@ def load(path: str) -> pd.DataFrame:
     return data_file
 
 
-def load_data() -> tuple[pd.DataFrame]:
+def normalize_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    df_min_max_scaled = df.copy()
+
+    for column in df_min_max_scaled.columns:
+        df_min_max_scaled[column] = (
+            df_min_max_scaled[column] - df_min_max_scaled[column].min()
+        ) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())
+
+    return df_min_max_scaled
+
+
+def load_data(df: pd.DataFrame) -> tuple[pd.DataFrame]:
     """load_data() -> tuple[pd.DataFrame]
     return a tuple of Dataframe, the first one
     is the km and the second one the price
     """
-    dataset = load("dataset/data.csv")
-    return (dataset["km"], dataset["price"])
+    return (df["km"], df["price"])
 
 
 if __name__ == "__main__":
--- src/train.py
+++ src/train.py
@@ -1,60 +1,87 @@
+import copy
+
+import numpy as np
 from linear_function import linear_function
-from numpy import ndarray
-from pandas import DataFrame
-from parse_data import load_data
+from parse_data import load, normalize_dataset
 
 
-def f(x):
-    return 60 * x
+class LinearRegression:
+    def __init__(self, learning_rate=0.01, n_iters=1500) -> None:
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
 
+    def _compute_cost(
+        theta0: int, theta1: int, x_train: np.ndarray, y_train: np.ndarray
+    ):
+        total_cost = 0
+        m = len(x_train)
+        for i in range(m):
+            f = linear_function(theta0, theta1, x_train[i])
+            total_cost += (f - y_train[i]) ** 2
+        return total_cost / (2 * m)
 
-def mean_squared_error(data: DataFrame, function) -> float:
-    sosr = 0
-    for x, y in data:
-        residual = y - f(x)
-        sosr += residual**2
-    return sosr / len(data)
+    def compute_gradient(
+        theta0: int, theta1: int, x_train: np.ndarray, y_train: np.ndarray
+    ) -> float:
+        m = len(x_train)
 
+        dj_theta0 = 0
+        dj_theta1 = 0
 
-def compute_cost(tetha0: int, tetha1: int, x_train: ndarray, y_train: ndarray):
-    total_cost = 0
-    m = len(x_train)
-    for i in range(m):
-        f = linear_function(tetha0, tetha1, x_train[i])
-        total_cost += (f - y_train[i]) ** 2
-    return total_cost / (2 * m)
+        for i in range(m):
+            prediction = linear_function(theta0, theta1, x_train[i])
 
+            theta0_i = (prediction - y_train[i]) * x_train[i]
+            theta1_i = prediction - y_train[i]
 
-def compute_gradient(
-    tetha0: int, tetha1: int, x_train: ndarray, y_train: ndarray
-) -> float:
-    m = len(x_train)
+            dj_theta0 += theta0_i
+            dj_theta1 += theta1_i
+            # print(f"cost: {compute_cost(theta0_i, theta1_i, x_train, y_train)}")
 
-    dj_tetha0 = 0
-    dj_tetha1 = 0
+        dj_theta0 /= m
+        dj_theta1 /= m
 
-    for i in range(m):
-        f = linear_function(tetha0, tetha1, x_train[i])
-        dj_tetha0 += (f - y_train[i]) * x_train[i]
-        dj_tetha1 += f - y_train[i]
+        return dj_theta0, dj_theta1
 
-    dj_tetha0 /= m
-    dj_tetha1 /= m
-    return dj_tetha0, dj_tetha1
+    def fit(self, x: np.ndarray, y: np.ndarray, theta1: float, theta0: float):
+        w = copy.deepcopy(theta1)
+        b = theta0
+
+        for i in range(self.n_iters):
+            dj_dw, dj_db = self._compute_gradient(b, w, x, y)
+
+            w = w - self.learning_rate * dj_dw
+            b = b - self.learning_rate * dj_db
+        return w, b
 
 
 def train(mileage: int) -> float:
-    tetha0 = 0  # b
-    tetha1 = 0  # w
-    # mileage = x
-    x_train, y_train = load_data()
+    theta0 = 0  # b
+    theta1 = 0  # w
+
+    df = load("dataset/data.csv")
+    normalized_df = df
+
+    x_train, y_train = (normalized_df["km"], normalized_df["price"])
     if x_train.shape != y_train.shape:
         raise AssertionError("Training data as incorrect shapes")
-    cost = compute_cost(tetha0, tetha1, x_train, y_train)
-    tmp_dj_dw, tmp_dj_db = compute_gradient(tetha0, tetha1, x_train, y_train)
-    print(f"Actual cost is: {cost}")
-    print(f"Gradient is {tmp_dj_dw} - {tmp_dj_db}")
+
+    model = LinearRegression()
+    w, b = model.fit(x_train, y_train, theta0, theta1)
+
+    # test train
+    m = x_train.shape[0]
+    predicted = np.zeros(m)
+
+    for i in range(m):
+        predicted[i] = w * x_train[i] + b
+
+    predict1 = 3.5 * w + b
+    print("For population = 35,000, we predict a profit of $%.2f" % (predict1 * 10000))
+
+    predict2 = 7.0 * w + b
+    print("For population = 70,000, we predict a profit of $%.2f" % (predict2 * 10000))
 
 
 if __name__ == "__main__":
-    train(4200)
+    train(13000)
